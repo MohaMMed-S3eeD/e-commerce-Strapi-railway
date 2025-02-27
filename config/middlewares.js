36beb27383a8d702c3b952a96f2cc90c2f36d947
Mohamed Seed <mosaeed7888@gmail.com>
2025-02-27 20:14:28 +0200

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,42 @@
+
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+            '*'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+            '*'
+          ],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          'frame-src': ["'self'", '*'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
