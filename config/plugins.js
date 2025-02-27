d857419859d8d12bfa02b91d8f98dbb7b2e86eb3
Mohamed Seed <mosaeed7888@gmail.com>
2025-02-27 20:40:05 +0200

    Update upload plugin configuration and add users-permissions plugin with jwtSecret

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,24 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+  // إعدادات الـ upload plugin مع Cloudinary
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    // ...
-  });
+  },
+  // إضافة الـ users-permissions plugin مع jwtSecret
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'P99zw1dNsRyBg/e2yUCeUA=='),
+    },
+  },
+});
