7386a0a511496593dcec1d41fc49cc4d5436f995
Mohamed Seed <mosaeed7888@gmail.com>
2025-02-27 20:55:33 +0200

    Add default values for admin JWT secret, API token salt, and transfer token salt

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'yourDefaultAdminSecretHere'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'yourDefaultApiTokenSaltHere'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'yourDefaultTransferSaltHere'),
     },
   },
   flags: {
